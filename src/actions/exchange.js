1afc503359a8391700ab2de9d74ed7922cbef13b
Geraldi <geraldi@coindoui-MacBookPro.local>
2020-01-30 18:58:20 +0700

    testing for action and store redux

--- src/actions/exchange.js
+++ src/actions/exchange.js
@@ -7,6 +7,30 @@ const exchangeAPI = axios.create({
 });
 
 /**
+ * Format any input to be accepted numeric format
+ * @param {string} amount - amount to be formatted
+ */
+const formatNumeric = amount => {
+  let preprocessedAmount = "";
+  // remove . to maximum one in amount
+  let dot = 0;
+  const amountInArray = amount.split("");
+
+  // check every char
+  for (let i = 0; i < amountInArray.length; i++) {
+    if (!dot && amountInArray[i] === ".") {
+      dot++;
+      preprocessedAmount += amountInArray[i];
+    }
+    if (amountInArray[i] !== ".") {
+      preprocessedAmount += amountInArray[i];
+    }
+  }
+
+  return preprocessedAmount.replace(/[^0-9.]{1,}/, "");
+};
+
+/**
  * Fetch latest exchange rates data from API.
  *
  * @param {function} dispatch - redux function
@@ -27,14 +51,14 @@ export const getLatestList = (baseCurrency = "USD") => dispatch =>
  * @param {string} amount - input number
  */
 export const changeAmountValue = amount => dispatch => {
-  const inputValue = amount === "" ? "0" : amount;
+  const inputValue = amount === "" ? "0" : formatNumeric(amount);
 
   return dispatch({
     type: EXCHANGE_TYPE.CHANGE_AMOUNT,
     payload:
       inputValue.substring(0, 1) === "0" && inputValue.length !== 1
         ? inputValue.substring(1)
-        : inputValue.match(/[0-9]*\.?[0-9]*/)
+        : inputValue
   });
 };
 
