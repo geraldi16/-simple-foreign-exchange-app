b85b2236ab47458dca383674cdce3633b800ff0c
Geraldi <geraldi@coindoui-MacBookPro.local>
2020-01-31 03:17:07 +0700

    provide responsive layout

--- src/component/InputAmount/style.js
+++ src/component/InputAmount/style.js
@@ -1,13 +1,21 @@
 import styled from "styled-components";
 import { Col, Row } from "react-flexbox-grid";
 
+import { media } from "../../utils/dimension";
+
 const Style = {
   Wrapper: styled(Row)`
-    margin-bottom: 20px;
-    padding: 10px 10px 0 10px;
     background: #f1f1f1;
     height: 100px;
-    border-radius: 5px;
+    width: 103%;
+    position: fixed;
+    top: 0;
+    padding: 10px 10px 0 10px;
+    ${media.tablet`
+      position: relative;
+      margin-bottom: 20px;
+      border-radius: 5px;
+    `}
   `,
   CurrencyName: styled(Col)`
     text-align: left;
@@ -24,6 +32,7 @@ const Style = {
   InputWrapper: styled(Col)`
     padding: 10px;
     text-align: right;
+    width: 100%;
   `,
   Input: styled.input`
     border: none;
@@ -32,6 +41,7 @@ const Style = {
     font-weight: bold;
     padding: 0 5px;
     text-align: right;
+    width: 100%;
     &:focus {
       outline: none;
     }
--- src/page/style.js
+++ src/page/style.js
@@ -1,15 +1,30 @@
 import styled from "styled-components";
 
 import background from "../img/background.jpg";
+import { media } from "../utils/dimension";
 
 const Style = {
   Wrapper: styled.div`
-    position: absolute;
-    width: 600px;
-    padding: 0 20px;
-    margin-left: 25vw;
-    margin-top: 5vh;
+    position: relative;
     z-index: 10;
+    ${media.tablet`
+        margin-left: 10vw;
+        margin-top: 5vh;
+        width: 600px;
+        padding: 0 20px;
+    `}
+    ${media.desktop`
+        margin-left: 20vw;
+        margin-top: 5vh;
+        width: 600px;
+        padding: 0 20px;
+    `}
+    ${media.laptopXL`
+        margin-left: 30vw;
+        margin-top: 5vh;
+        width: 600px;
+        padding: 0 20px;
+    `}
   `,
   Background: styled.div`
     background: url(${background});
